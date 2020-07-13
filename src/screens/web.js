3d9519329b2115448d22ab058fc5f55eaba6aee3
kingofdevs <40970351+kingofdevs@users.noreply.github.com>
2020-07-13 16:29:38 +0800

    Update web.js

--- src/screens/web.js
+++ src/screens/web.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Dimensions, StyleSheet, View, Image, Text } from 'react-native';
 import { p } from '../common/normalize';
 import colors from '../common/colors';
@@ -6,16 +6,17 @@ import images from '../common/images';
 import Webview from 'react-native-webview';
 
 const { width, height } = Dimensions.get('window');
+const uri = 'https://whichbrowser.net/'
 
 export const WebScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={{ width: width-p(30), height: height-p(60), borderWidth: 2, borderColor: 'green', backgroundColor: '#000' }}>
+      <View style={styles.web}>
         <Webview 
-          source={{uri: 'https://whichbrowser.net/' }}
+          source={uri}
         />
       </View>
-      <Text style={{ color: '#fff'}}>{'https://whichbrowser.net/'}</Text>
+      <Text style={{ color: '#fff'}}>{uri}</Text>
       <Image source={images.logo} />
     </View>
   );
@@ -27,6 +28,13 @@ const styles = StyleSheet.create({
     backgroundColor: colors.dark_blue,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  web: {
+    width: width - p(30),
+    height: height - p(60),
+    borderWidth: 2,
+    borderColor: 'green',
+    backgroundColor: '#000'
   },
   loading: {
     width: p(50),
